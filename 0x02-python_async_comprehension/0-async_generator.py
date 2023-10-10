dca275b34f181f3e955a2e622cd16a4d109e4977
@steve <asteve12335@gmail.com>
2023-10-10 20:46:58 +0100

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is a pythn 3 asynchronous generator function.
+"""
+import asyncio
+import typing
+import random
+
+
+async def async_generator() -> typing.AsyncGenerator[float]:
+    """
+    Generates a sequence of 10 random floats between 0 and 10,
+    pausing for 1 second between each number.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
