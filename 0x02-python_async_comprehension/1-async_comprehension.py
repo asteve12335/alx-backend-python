70389afb7e57a6472688ece874a6bad89169ff46
@steve <asteve12335@gmail.com>
2023-10-10 22:07:44 +0100

    Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,5 +10,9 @@ import random
 async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension():
-    random_numbers = [random.choice(async_generator) async for i in range(10)]
+    """
+    Collect 10 random numbers using an async comprehensing over
+    async_generator, then return the 10 random numbers.
+    """
+    random_numbers = [i async for i in async_generator]
     return random_numbers
